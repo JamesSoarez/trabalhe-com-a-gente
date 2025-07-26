ce1610ccae7a1ac6992bd55dfa38b6a26480bea5
JamesSoarez <jamesoarez@gmail.com>
2025-07-26 17:01:47 -0300

    feat: bind search input and button to component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,18 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'challenge-git-search';
+  searchTerm: String = ""; //-> guardar valor no imput
+
+  treatSearch(): void{ //-> método chamado ao clicar no botão
+    console.log("Busca por: ", this.searchTerm);  
+  }
 }
