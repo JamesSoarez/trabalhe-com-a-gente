ebcff1e0b83b00b829101748dc5089b0e98b143c
JamesSoarez <jamesoarez@gmail.com>
2025-07-26 17:26:31 -0300

    feat: call github api on search button click

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
+import { GithubService } from './services/github.service';
 
 @Component({
   selector: 'app-root',
@@ -9,10 +10,19 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
+
 export class AppComponent {
-  searchTerm: String = ""; //-> guardar valor no imput
+  searchTerm: string = ""; //-> guardar valor no imput
+
+  //Serviço passado no Construtor
+  constructor(private githubService: GithubService) {}
 
   treatSearch(): void{ //-> método chamado ao clicar no botão
-    console.log("Busca por: ", this.searchTerm);  
+    this.githubService.searchRepositories(this.searchTerm)
+    .subscribe(response => {
+      //Resposta no Console
+      console.log("Dados recebidos da API GitHub: ");
+      console.log(response);
+    }); 
   }
 }
