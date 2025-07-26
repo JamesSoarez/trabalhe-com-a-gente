dce64396753143077d7940577a969f9199cb00ac
JamesSoarez <jamesoarez@gmail.com>
2025-07-26 19:26:36 -0300

    feat: render repository list from api data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,28 +1,29 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { GithubService } from './services/github.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, FormsModule],
+  imports: [CommonModule, RouterOutlet, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 
 export class AppComponent {
-  searchTerm: string = ""; //-> guardar valor no imput
+  searchTerm: string = "";//-> guardar valor no imput
+  repositories: any[] = [];//-> guardar lista contida em Items
 
   //Serviço passado no Construtor
   constructor(private githubService: GithubService) {}
 
-  treatSearch(): void{ //-> método chamado ao clicar no botão
+  treatSearch(): void{//-> método chamado ao clicar no botão
     this.githubService.searchRepositories(this.searchTerm)
     .subscribe(response => {
-      //Resposta no Console
-      console.log("Dados recebidos da API GitHub: ");
       console.log(response);
+      this.repositories = response.items;
     }); 
   }
 }
