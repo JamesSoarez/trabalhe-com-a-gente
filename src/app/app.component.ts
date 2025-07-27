ca9955d9b65c30cf555d858f2ad52a36c3a84141
JamesSoarez <jamesoarez@gmail.com>
2025-07-27 17:14:43 -0300

    feat(ui): refactor app component with angular material

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,62 +1,75 @@
 import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 import { GithubService } from './services/github.service';
+
+//os imports do Angular Material
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, FormsModule],
+  imports: [
+    CommonModule,
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatCardModule,
+    MatProgressSpinnerModule,
+    MatIconModule
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
-
 export class AppComponent {
-  searchTerm: string = "";//-> guardar valor no imput
-  repositories: any[] = [];//-> guardar lista contida em items
-
-  //Paginação
+  searchTerm: string = '';
+  repositories: any[] = [];
   currentPage: number = 1;
   totalCount: number = 0;
+  errorMessage: string | null = null;
+  
+  isLoading: boolean = false; // aqui será controlado o circulo de carregamento (spinner)
 
-  //Serviço passado no Construtor
   constructor(private githubService: GithubService) {}
 
-  //Resetar página
   treatSearch(): void {
-    this.currentPage = 1; //a cada nova busca, retorna para a primeira página
+    if (!this.searchTerm.trim()) return; // se o campo estiver vazio, não realizará busca
+    this.currentPage = 1;
     this.executeSearch();
   }
-
-  private executeSearch(): void {//-> método chamado ao clicar no botão
+  
+  private executeSearch(): void {
+    this.errorMessage = null;
+    this.isLoading = true; //mostra o circulo de carregamento (spinner)
     this.githubService.searchRepositories(this.searchTerm, this.currentPage)
-    .subscribe({
-      
-      //Chamada realizada
-      next: (response) => { 
-        this.repositories = response.items; //salva items da resposta 
-        this.totalCount = response.total_count;//Guarda o total de resultados
-      },
-
-      //Erro ao realizar a chamada
-      error: (err) => {
-        console.error("ERRO AO FAZER A BUSCA:", err);
-        this.repositories = []; //resultados de dados antigos são limpados
-        this.currentPage = 0;
-      }
-
-    }); 
+      .subscribe({
+        next: (response) => {
+          this.repositories = response.items;
+          this.totalCount = response.total_count;
+          this.isLoading = false; //esconde o circulo de carregamento (spinner)
+        },
+        error: (err) => {
+          console.error("ERRO AO FAZER A BUSCA:", err);
+          this.repositories = [];
+          this.totalCount = 0;
+          this.errorMessage = "Erro ao buscar dados. Você pode ter atingido o limite de requisições. Tente novamente em um minuto.";
+          this.isLoading = false; //se houver erro, também esconde o circulo de carregamento (spinner)
+        }
+      });
   }
 
-  //Mudar de página
-  nextPage(): void {
+  nextPage(): void { // próxima página
     this.currentPage++;
     this.executeSearch();
   }
 
-  //página anterior
-  previousPage(): void {
+  previousPage(): void { //página anterior
     this.currentPage--;
     this.executeSearch();
   }
