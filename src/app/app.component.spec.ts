af76a2d4f1561b07d8bdffe35a0267fa4abbe854
JamesSoarez <jamesoarez@gmail.com>
2025-07-27 04:24:21 -0300

    test: add unit and integration tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,29 +1,53 @@
-import { TestBed } from '@angular/core/testing';
+import { TestBed, ComponentFixture } from '@angular/core/testing';
+import { FormsModule } from '@angular/forms';
+import { of } from 'rxjs';
+
 import { AppComponent } from './app.component';
+import { GithubService } from './services/github.service';
+import { provideHttpClientTesting } from '@angular/common/http/testing';
 
 describe('AppComponent', () => {
+  let component: AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
+  let GithubServiceSpy: jasmine.SpyObj<GithubService>;
+
   beforeEach(async () => {
+
+    const spy = jasmine.createSpyObj("GithubService", ["searchRepositories"]);
+
     await TestBed.configureTestingModule({
-      imports: [AppComponent],
+      imports: [
+        AppComponent,
+        FormsModule
+      ],
+      providers: [
+        { provide: GithubService, useValue: spy },
+        provideHttpClientTesting()
+      ]
     }).compileComponents();
+
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.componentInstance;
+
+    GithubServiceSpy = TestBed.inject(GithubService) as jasmine.SpyObj<GithubService>;
   });
 
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
+    expect(component).toBeTruthy();
   });
 
-  it(`should have the 'challenge-git-search' title`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('challenge-git-search');
-  });
+  it(`should call githubService.searchRepositories when search button is clicked`, () => {
+    const testSearchTerm = "angular";
+    
+    GithubServiceSpy.searchRepositories.and.returnValue(of({ items: [], total_count: 0 }));
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
+    component.searchTerm = testSearchTerm;
     fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, challenge-git-search');
+
+    const searchButton = fixture.nativeElement.querySelector("button");
+    searchButton.click();
+
+    expect(GithubServiceSpy.searchRepositories).toHaveBeenCalledWith(testSearchTerm, 1);
   });
+
 });
--- src/app/services/github.service.spec.ts
+++ src/app/services/github.service.spec.ts
@@ -1,16 +1,62 @@
 import { TestBed } from '@angular/core/testing';
+import { HttpClient } from '@angular/common/http';
+import { of } from 'rxjs';
 
 import { GithubService } from './github.service';
+import { GITHUB_TOKEN } from '../api-keys';
 
 describe('GithubService', () => {
   let service: GithubService;
+  let httpClientSpy: jasmine.SpyObj<HttpClient>;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    const spy = jasmine.createSpyObj('HttpClient', ['get']);
+
+    TestBed.configureTestingModule({
+      providers: [
+        GithubService,
+        { provide: HttpClient, useValue: spy }
+      ]
+    });
     service = TestBed.inject(GithubService);
+    httpClientSpy = TestBed.inject(HttpClient) as jasmine.SpyObj<HttpClient>;
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should call http.get with correct URL, params, and headers', () => {
+    const testQuery = 'typescript';
+    const testPage = 2;
+    const expectedUrl = "https://api.github.com/search/repositories";
+
+    httpClientSpy.get.and.returnValue(of({ items: [] }));
+
+    service.searchRepositories(testQuery, testPage).subscribe();
+
+    expect(httpClientSpy.get).toHaveBeenCalledTimes(1);
+
+    const mostRecentCall = httpClientSpy.get.calls.mostRecent();
+    
+    const url = mostRecentCall.args[0];
+    const options = mostRecentCall.args[1];
+
+    expect(url).toEqual(expectedUrl);
+
+    expect(options).withContext('Options object should have been provided').toBeDefined();
+    
+    if (options) {
+      expect(options.params).withContext('Params should be defined').toBeDefined();
+
+      expect(Object.keys(options.params!)).toContain('q');
+      expect(Object.keys(options.params!)).toContain('page');
+
+      expect((options.params! as any).q).toEqual(testQuery);
+      expect((options.params! as any).page).toEqual(testPage.toString());
+
+      expect(options.headers).withContext('Headers should be defined').toBeDefined();
+      expect((options.headers as any).get('Authorization')).toEqual(`Bearer ${GITHUB_TOKEN}`);
+    }
   });
 });
